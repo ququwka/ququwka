4a39769ef7aa59cd65fa6f68ef69354d012c0905
ququwka <99955711+ququwka@users.noreply.github.com>
2022-02-18 14:49:52 +0600

    Add files via upload

---
+++ Baha/Baha/src/main/java/com/example/Baha/BahaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Baha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahaApplication.class, args);
+	}
+
+}
---
+++ Baha/Baha/src/test/java/com/example/Baha/BahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Baha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
